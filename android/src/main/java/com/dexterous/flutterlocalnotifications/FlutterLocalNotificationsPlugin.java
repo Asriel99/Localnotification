5df261ee87e55a2a69bb80e50994c637ba9184eb
Asriel <onube53@gmail.com>
2020-09-03 15:17:17 -0300

    master

--- android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -199,21 +199,21 @@ public class FlutterLocalNotificationsPlugin implements MethodCallHandler {
     return notification;
     }
 
-//    private  void setSmallIcon(Context context, NotificationDetails notificationDetails, NotificationCompat.Builder builder) {
-//        if (!StringUtils.isNullOrEmpty(notificationDetails.icon)) {
-//            builder.setSmallIcon(getDrawableResourceId(context, notificationDetails.icon));
-//        } else {
-//            SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE);
-//            String defaultIcon = sharedPreferences.getString(DEFAULT_ICON, null);
-//            if (StringUtils.isNullOrEmpty(defaultIcon)) {
-//                // for backwards compatibility: this is for handling the old way references to the icon used to be kept but should be removed in future
-//                builder.setSmallIcon(notificationDetails.iconResourceId);
-//
-//            } else {
-//                builder.setSmallIcon(getDrawableResourceId(context, defaultIcon));
-//            }
-//        }
-//    }
+    private  void setSmallIcon(Context context, NotificationDetails notificationDetails, NotificationCompat.Builder builder) {
+        if (!StringUtils.isNullOrEmpty(notificationDetails.icon)) {
+            builder.setSmallIcon(getDrawableResourceId(context, notificationDetails.icon));
+        } else {
+            SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE);
+            String defaultIcon = sharedPreferences.getString(DEFAULT_ICON, null);
+            if (StringUtils.isNullOrEmpty(defaultIcon)) {
+                // for backwards compatibility: this is for handling the old way references to the icon used to be kept but should be removed in future
+                builder.setSmallIcon(notificationDetails.iconResourceId);
+
+            } else {
+                builder.setSmallIcon(getDrawableResourceId(context, defaultIcon));
+            }
+        }
+    }
 
     @NonNull
      Gson buildGson() {
